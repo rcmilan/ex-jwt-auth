f092614c43cce4e38a02da3c4f088a1941f90bc6
rcmilan <13418519+rcmilan@users.noreply.github.com>
2022-12-16 09:32:48 -0300

    ClaimsIdentity

--- MyServerApi/Program.cs
+++ MyServerApi/Program.cs
@@ -1,7 +1,33 @@
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+var rsaKey = RSA.Create();
+rsaKey.ImportRSAPrivateKey(File.ReadAllBytes("myRsaKey"), out _);
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("jwt")
+    .AddJwtBearer("jwt", o => {
+    });
 
 var app = builder.Build();
 
 app.MapGet("/", () => "hello world");
+app.MapGet("/jwt", () =>
+{
+    var handler = new JsonWebTokenHandler();
+    var token = handler.CreateToken(new SecurityTokenDescriptor()
+    {
+        Subject = new ClaimsIdentity(new[]
+        {
+            new Claim("sub", Guid.NewGuid().ToString()),
+            new Claim("user", "huehuehue br"),
+        })
+    });
+});
 
 app.Run();
